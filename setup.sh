1ce6eb82457533d81926f6f04950b5fa815b3406
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-01-25 21:50:40 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+
+echo "sda1 is root; sda2 is swap; in root"
+mkfs.ext4 -L nixos /dev/sda1
+mkswap -L swap /dev/sda2
+swapon /dev/sda2
+mount /dev/disk/by-label/nixos /mnt
+nixos-generate-config --root /mnt
+git clone https://github.com/gideonthomasd/configuration.git
+cd configuration
+cp configuration.nix /mnt/etc/nixos/configuration.nix
+nixos-install
 
